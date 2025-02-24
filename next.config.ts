f77550ef3c5258a14c1b982883236b51a679e92e
Dr.Awesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2025-02-23 17:28:48 -0700

    Phase 2 of next js migration

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
-  basePath: '/ap-tracker',
+  basePath: '/ap-tracker'
 }
  
 export default nextConfig
